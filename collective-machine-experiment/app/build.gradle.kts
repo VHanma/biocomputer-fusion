46896686427189bcc62d6d699d0c44e263bd998a
VHanma <morgandeavon@gmail.com>
2026-08-10 05:54:35 -0500

    Version Collective Machine Experiment V2

--- collective-machine-experiment/app/build.gradle.kts
+++ collective-machine-experiment/app/build.gradle.kts
@@ -7,11 +7,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.vaan.collectivemachine"
+        applicationId = "com.vaan.collectivemachine.v2"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
